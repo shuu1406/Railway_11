01c0cb410f667a4f4e73e42a55e6c04dc5d0ad42
shuu1406 <nghiadoan05@gmail.com>
2021-05-09 12:08:05 +0700

    SQL

--- SQL/Assignment1/Testing_System_Assignment_1.sql
+++ SQL/Assignment1/Testing_System_Assignment_1.sql
@@ -3,67 +3,78 @@ CREATE DATABASE TestManagement;
 USE TestManagement;
 
 CREATE TABLE Departments (
-	department_id		INT,
-    department_name		VARCHAR(50)
+	department_id		TINYINT AUTO_INCREMENT PRIMARY KEY, 
+    department_name		NVARCHAR(50) NOT NULL UNIQUE KEY
     );
-CREATE TABLE Position (
-	position_id			INT,
-    position_name		VARCHAR(50)
+CREATE TABLE `Position` (
+	position_id			TINYINT AUTO_INCREMENT PRIMARY KEY,
+    position_name		ENUM('Dev','Test','Scrum Master', 'PM') NOT NULL
     );
-CREATE TABLE Account1 (
-	account_id			INT,
+CREATE TABLE `Account` (
+	account_id			INT AUTO_INCREMENT PRIMARY KEY,
     email				VARCHAR(50),
     username			VARCHAR(50),
-    fullname			VARCHAR(50),
+    fullname			NVARCHAR(50),
     department_id		INT,
     position_id			INT,
-    create_date			DATE
+    create_date			DATE,
+    FOREIGN KEY (department_id) REFERENCES  Departments  (department_id),
+	FOREIGN KEY (position_id) REFERENCES  `Position` (position_id)
+
 );
-CREATE TABLE Group1 (
-	group_id			INT,
-    group_name			VARCHAR(50),
+CREATE TABLE `Group` (
+	group_id			INT AUTO_INCREMENT PRIMARY KEY,
+    group_name			NVARCHAR(50),
     creator_id			INT,
     create_date		    DATE
 );
 CREATE TABLE GroupAccount (
 	group_id			INT,
     account_id			INT,
-    join_date			DATE
+    join_date			DATE,
+    FOREIGN KEY (group_id) REFERENCES `Group` (group_id),
+	FOREIGN KEY (account_id) REFERENCES  `Account`(account_id)
+
 );
 CREATE TABLE TypeQuestion (
-	type_id				INT,
-    type_name			VARCHAR(50)
+	type_id				INT AUTO_INCREMENT PRIMARY KEY,
+    type_name			ENUM('Essay','Multiple-Choice')
 );
 CREATE TABLE CategoryQuestion (
-	category_id			INT,
-    category_name		VARCHAR(50)
+	category_id			INT AUTO_INCREMENT PRIMARY KEY,
+    category_name		NVARCHAR(50)
 );
 CREATE TABLE Question (
-	question_id 		INT,
-    content				VARCHAR(50),
+	question_id 		INT AUTO_INCREMENT PRIMARY KEY,
+    content				NVARCHAR(50),
     category_id			INT,
     type_id				INT,
     creator_id			INT,
-    create_date			DATE
-    
+    create_date			DATE,
+    FOREIGN KEY ( creator_id ) REFERENCES `Group`( creator_id ),
+    FOREIGN KEY ( create_date) REFERENCES `Group`( create_date)
 );
 CREATE TABLE Answer (
-	answer_id 			INT,
-    content 			VARCHAR(50),
+	answer_id 			INT AUTO_INCREMENT PRIMARY KEY,
+    content 			NVARCHAR(50),
     question_id			INT,
-    isCorrect			ENUM('correct','incorrect')
+    isCorrect			ENUM('correct','incorrect'),
+    FOREIGN KEY (question_id) REFERENCES Question(question_id)
 );
 CREATE TABLE Exam (
-	exam_id				INT,
+	exam_id				INT AUTO_INCREMENT PRIMARY KEY,
     code1				INT,
-    title				VARCHAR(50),
+    title				NVARCHAR(50),
     category_id			INT,
     duration			TIME,
-    creat_id			INT,
-    creat_date			DATE
+    creator_id			INT,
+    create_date			DATE,
+    FOREIGN KEY ( creator_id ) REFERENCES `Group`( creator_id ),
+    FOREIGN KEY ( create_date) REFERENCES `Group`( create_date)
 );
 CREATE TABLE ExemQuestion (
 	exam_id				INT,
-    question_id			INT
+    question_id			INT,
+    FOREIGN KEY (exam_id) REFERENCES Exam(exam_id)
 );
     
