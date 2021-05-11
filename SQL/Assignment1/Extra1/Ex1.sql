12c775012763d647260cb13a93f3f8543fb83133
shuu1406 <nghiadoan05@gmail.com>
2021-05-11 08:39:25 +0700

    SQL

--- SQL/Assignment1/Extra1/Ex1.sql
+++ SQL/Assignment1/Extra1/Ex1.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS Quanli;
-CREATE DATABASE IF NOT EXISTS Quanli;
+DROP DATABASE IF EXISTS QuanliFresher;
+CREATE DATABASE IF NOT EXISTS QuanliFresher;
 USE Quanli;
 DROP TABLE IF EXISTS Trainee;
 CREATE TABLE Trainee
@@ -8,10 +8,14 @@ TraineeID		TINYINT AUTO_INCREMENT NOT NULL UNIQUE KEY,
 FullName		NVARCHAR(50) NOT NULL,
 BirthDate		DATE NOT NULL,
 Gender			ENUM('male','female','unknown'),
-ET_IQ			TINYINT CHECK( 0<ET_IQ && 20 >ET_IQ) NOT NULL,
-ET_Gmath		TINYINT CHECK(0<ET_Gmath && 20>ET_Gmath) NOT NULL,
-ET_English		TINYINT CHECK(0<ET_English && 50>ET_English) NOT NULL,
-TrainingClass	INT,
-EvaluationNotes CHAR,
-VTI_Account		VARCHAR(50) NOT NULL UNIQUE KEY
+ET_IQ			TINYINT CHECK( 0<=ET_IQ && 20 >=ET_IQ) NOT NULL,
+ET_Gmath		TINYINT CHECK(0<=ET_Gmath && 20>=ET_Gmath) NOT NULL,
+ET_English		TINYINT CHECK(0<=ET_English && 50>=ET_English) NOT NULL,
+TrainingClass	CHAR,
+EvaluationNotes TEXT
 );
+ALTER TABLE Trainee
+ADD VTI_Account CHAR NOT NULL UNIQUE KEY;
+INSERT INTO Trainee(FullName)
+VALUES("Nghia")
+
