a71a9b4f62591a60b81f3cd7fe50be72ef1095a1
shuu1406 <nghiadoan05@gmail.com>
2021-05-11 10:57:33 +0700

    SQL

--- SQL/Assignment1/Testing_System_Assignment_1.sql
+++ SQL/Assignment1/Testing_System_Assignment_1.sql
@@ -1,80 +1,94 @@
-DROP DATABASE TestManagement;
-CREATE DATABASE TestManagement;
-USE TestManagement;
+DROP DATABASE IF EXISTS Testing_System_Assignment_1;
+CREATE DATABASE IF NOT EXISTS Testing_System_Assignment_1;
+USE Testing_System_Assignment_1;
 
-CREATE TABLE Departments (
-	department_id		TINYINT AUTO_INCREMENT PRIMARY KEY, 
-    department_name		NVARCHAR(50) NOT NULL UNIQUE KEY
-    );
-CREATE TABLE `Position` (
-	position_id			TINYINT AUTO_INCREMENT PRIMARY KEY,
-    position_name		ENUM('Dev','Test','Scrum Master', 'PM') NOT NULL
-    );
+CREATE TABLE Department (
+	department_id 			TINYINT AUTO_INCREMENT PRIMARY KEY ,
+	department_name 		NVARCHAR(50) UNIQUE KEY NOT NULL
+);
+
+CREATE TABLE Position (
+	position_id 			TINYINT AUTO_INCREMENT PRIMARY KEY ,
+	position_name			ENUM('Dev','Test', 'Scrum Master', 'PM')
+);
+   
 CREATE TABLE `Account` (
-	account_id			INT AUTO_INCREMENT PRIMARY KEY,
-    email				VARCHAR(50),
-    username			VARCHAR(50),
-    fullname			NVARCHAR(50),
-    department_id		INT,
-    position_id			INT,
-    create_date			DATE,
-    FOREIGN KEY (department_id) REFERENCES  Departments  (department_id),
-	FOREIGN KEY (position_id) REFERENCES  `Position` (position_id)
-
+	account_id 			SMALLINT AUTO_INCREMENT PRIMARY KEY ,
+	email 				VARCHAR(50) NOT NULL UNIQUE KEY,
+	user_name 			VARCHAR(50) NOT NULL,
+	full_name 			NVARCHAR(50) NOT NULL,
+	department_id 		TINYINT NOT NULL,
+	position_id 		TINYINT NOT NULL,
+	create_date 		DATE,
+    FOREIGN KEY (department_id) REFERENCES Department(department_id),
+	FOREIGN KEY (position_id) REFERENCES Position (position_id)    
 );
-CREATE TABLE `Group` (
-	group_id			INT AUTO_INCREMENT PRIMARY KEY,
-    group_name			NVARCHAR(50),
-    creator_id			INT,
-    create_date		    DATE
-);
+ 
+ CREATE TABLE `Group` (
+	group_id 		TINYINT AUTO_INCREMENT PRIMARY KEY,
+	group_name 		VARCHAR(50) NOT NULL UNIQUE KEY,
+	creator_id 		SMALLINT NOT NULL,
+	create_date DATE,
+    FOREIGN KEY (creator_id) REFERENCES `Account`(account_id)
+ );
+ 
 CREATE TABLE GroupAccount (
-	group_id			INT,
-    account_id			INT,
-    join_date			DATE,
-    FOREIGN KEY (group_id) REFERENCES `Group` (group_id),
-	FOREIGN KEY (account_id) REFERENCES  `Account`(account_id)
-
+	group_id 		TINYINT NOT NULL,
+	account_id 		SMALLINT NOT NULL,
+	join_date 		DATE,
+    PRIMARY KEY(group_id,account_id),
+    FOREIGN KEY (group_id) REFERENCES `Group`(group_id),
+    FOREIGN KEY (account_id) REFERENCES `Account`(account_id)
 );
+ -- (1,2),(1,1),(1,3),(1,4)
+ 
 CREATE TABLE TypeQuestion (
-	type_id				INT AUTO_INCREMENT PRIMARY KEY,
-    type_name			ENUM('Essay','Multiple-Choice')
+	type_id 		TINYINT AUTO_INCREMENT PRIMARY KEY ,
+	type_name 		ENUM('Essay','Multiple-Choice')
 );
+  
 CREATE TABLE CategoryQuestion (
-	category_id			INT AUTO_INCREMENT PRIMARY KEY,
-    category_name		NVARCHAR(50)
+	category_id 	TINYINT AUTO_INCREMENT PRIMARY KEY ,
+	category_name 	NVARCHAR(50)
 );
+ 
 CREATE TABLE Question (
-	question_id 		INT AUTO_INCREMENT PRIMARY KEY,
-    content				NVARCHAR(50),
-    category_id			INT,
-    type_id				INT,
-    creator_id			INT,
-    create_date			DATE,
-    FOREIGN KEY ( creator_id ) REFERENCES `Group`( creator_id ),
-    FOREIGN KEY ( create_date) REFERENCES `Group`( create_date)
+	question_id 	SMALLINT AUTO_INCREMENT PRIMARY KEY ,
+	content 		TEXT,
+	category_id 	TINYINT,
+	type_id 		TINYINT,
+	creator_id 		SMALLINT,
+	create_date 	DATE,
+    FOREIGN KEY (type_id) REFERENCES TypeQuestion(type_id),
+    FOREIGN KEY (creator_id) REFERENCES `Account`(account_id),
+	FOREIGN KEY (category_id) REFERENCES `CategoryQuestion`(category_id)
 );
+ 
 CREATE TABLE Answer (
-	answer_id 			INT AUTO_INCREMENT PRIMARY KEY,
-    content 			NVARCHAR(50),
-    question_id			INT,
-    isCorrect			ENUM('correct','incorrect'),
-    FOREIGN KEY (question_id) REFERENCES Question(question_id)
+	answer_id 		SMALLINT AUTO_INCREMENT PRIMARY KEY,
+	content 		TEXT,
+	question_id 	SMALLINT,
+	is_correct 		BOOLEAN,
+    -- BIT,BOOLEAN,Enum('True','False')
+	FOREIGN KEY (question_id) REFERENCES Question(question_id)
 );
+-- (1,2),(1,3)
 CREATE TABLE Exam (
-	exam_id				INT AUTO_INCREMENT PRIMARY KEY,
-    code1				INT,
-    title				NVARCHAR(50),
-    category_id			INT,
-    duration			TIME,
-    creator_id			INT,
-    create_date			DATE,
-    FOREIGN KEY ( creator_id ) REFERENCES `Group`( creator_id ),
-    FOREIGN KEY ( create_date) REFERENCES `Group`( create_date)
+	exam_id 		SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	code 			CHAR(50) NOT NULL,
+	title 			NVARCHAR(50) NOT NULL,
+	category_id 	TINYINT NOT NULL,
+	duration 		TINYINT UNSIGNED NOT NULL, 
+	creator_id 		SMALLINT NOT NULL,
+	create_date 	DATE,
+    FOREIGN KEY (category_id) REFERENCES `CategoryQuestion`(category_id),
+    FOREIGN KEY (creator_id) REFERENCES `Account`(account_id)
 );
-CREATE TABLE ExemQuestion (
-	exam_id				INT,
-    question_id			INT,
-    FOREIGN KEY (exam_id) REFERENCES Exam(exam_id)
+CREATE TABLE ExamQuestion (
+	exam_id 		SMALLINT UNSIGNED NOT NULL,
+	question_id 	SMALLINT NOT NULL,
+	PRIMARY KEY (exam_id,question_id),
+	FOREIGN KEY (exam_id) REFERENCES Exam(exam_id),
+	FOREIGN KEY (question_id) REFERENCES `Question`(question_id)
 );
-    
+ 
